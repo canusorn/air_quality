993f78355e09c281f8cebf91fdb32625df5cb0a8
canusorn <anusorn1998@gmail.com>
2023-03-09 17:09:18 +0700

    Update ESP32_Air_quality.ino

--- ESP32_Air_quality/ESP32_Air_quality.ino
+++ ESP32_Air_quality/ESP32_Air_quality.ino
@@ -39,10 +39,31 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+#include <Wire.h>
+#include <SFE_MicroOLED.h>
+#include <PMS.h>
+#include <DHT.h>
+#include <HardwareSerial.h>
+
+HardwareSerial SerialPort(1); // use UART1
+PMS pms(SerialPort);
+PMS::DATA data;
+
+#define DHTPIN 23
+#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#define PIN_RESET -1
+#define DC_JUMPER 0
+MicroOLED oled(PIN_RESET, DC_JUMPER);
+
+unsigned long previousMillis = 0;
+
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "ssid";
-char pass[] = "pass";
+char ssid[] = "G6PD_2.4G";
+char pass[] = "570610193";
 
 BLYNK_CONNECTED()
 {
@@ -53,11 +74,11 @@ BLYNK_WRITE(V3)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(16, HIGH);
+    digitalWrite(16, LOW );
   }
   else
   {
-    digitalWrite(16, LOW);
+    digitalWrite(16, HIGH);
   }
 }
 
@@ -65,55 +86,55 @@ BLYNK_WRITE(V4)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(17, HIGH);
+    digitalWrite(17, LOW );
   }
   else
   {
-    digitalWrite(17, LOW);
+    digitalWrite(17, HIGH);
   }
 }
 BLYNK_WRITE(V5)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(18, HIGH);
+    digitalWrite(18, LOW );
   }
   else
   {
-    digitalWrite(18, LOW);
+    digitalWrite(18, HIGH);
   }
 }
 BLYNK_WRITE(V6)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(19, HIGH);
+    digitalWrite(19, LOW );
   }
   else
   {
-    digitalWrite(19, LOW);
+    digitalWrite(19, HIGH);
   }
 }
 BLYNK_WRITE(V7)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(21, HIGH);
+    digitalWrite(21, LOW );
   }
   else
   {
-    digitalWrite(21, LOW);
+    digitalWrite(21, HIGH);
   }
 }
 BLYNK_WRITE(V8)
 {
   if (param.asInt() == 1)
   {
-    digitalWrite(22, HIGH);
+    digitalWrite(22, LOW );
   }
   else
   {
-    digitalWrite(22, LOW);
+    digitalWrite(22, HIGH);
   }
 }
 
@@ -121,19 +142,60 @@ void setup()
 {
   // Debug console
   Serial.begin(115200);
+  SerialPort.begin(9600, SERIAL_8N1, 25, 27);
 
-  //  digitalWrite(16, LOW);
-  //  pinMode(16, OUTPUT);
+  dht.begin();
+  Wire.begin();
+
+  oled.begin();
+  oled.clear(PAGE);
+  oled.clear(ALL);
+
+  digitalWrite(16, HIGH);
+  digitalWrite(17, HIGH);
+  digitalWrite(18, HIGH);
+  digitalWrite(19, HIGH);
+  digitalWrite(21, HIGH);
+  digitalWrite(22, HIGH);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(22, OUTPUT);
 
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop()
 {
-
-  Blynk.virtualWrite(V0, 54);
-  Blynk.virtualWrite(V1, 26.2);
-  Blynk.virtualWrite(V2, 57.9);
   Blynk.run();
-  delay(1000);
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 2000) {
+    previousMillis = currentMillis;
+
+    float humid = dht.readHumidity();
+    float temp = dht.readTemperature();
+
+    Serial.print(F("Humidity: "));
+    Serial.print(humid);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(temp);
+    Serial.println(F("°C "));
+
+    Blynk.virtualWrite(V1, temp);
+    Blynk.virtualWrite(V2, humid);
+    
+  }
+
+  //------get data from PMS7003------
+  if (pms.read(data))
+  {
+    Blynk.virtualWrite(V0, data.PM_AE_UG_2_5);
+    Serial.print(F("PM2.5: "));
+    Serial.print(data.PM_AE_UG_2_5);
+    Serial.println(F("ug/m3 "));
+  }
+
+
 }
