799342c4f8c9f0a4b69a2ca68992f58e313bee7c
canusorn <anusorn1998@gmail.com>
2023-03-13 15:28:44 +0700

    add comment

--- ESP32_Air_quality/ESP32_Air_quality.ino
+++ ESP32_Air_quality/ESP32_Air_quality.ino
@@ -1,5 +1,7 @@
 /*************************************************************
+  สำหรับบอร์ด ESP32
 
+  การต่อสาย
   PMS7003
   TX  -> 25
   RX  -> 27
@@ -7,117 +9,119 @@
   VCC -> VCC
 
   DHT -> 26
+
+   ไลบรารี่เพิ่มเติม
+   1.PMS โหลดจาก library manager
+   2.DHT โหลดจาก library manager
+   3.blynk โหลดจาก library manager
+
  *************************************************************/
 
-/* Comment this out to disable prints and save space */
+/* ดีบัก Blynk ผ่าน Serial */
 #define BLYNK_PRINT Serial
 
-/* Fill in information from Blynk Device Info here */
+/* ข้อมูลจาก Blynk */
 #define BLYNK_TEMPLATE_ID "TMPLHZXTQTqu"
 #define BLYNK_TEMPLATE_NAME "Air quality"
 #define BLYNK_AUTH_TOKEN "f4FQUVZN0zuZ7F49CbB8KJt7yTreeDUf"
 
+#include <WiFi.h>             // ไวไฟ
+#include <WiFiClient.h>       // ไวไฟ
+#include <BlynkSimpleEsp32.h> // Blynk
+#include <PMS.h>              // PMS7003 sensor
+#include <DHT.h>              // DHT
+#include <HardwareSerial.h>   // Uart กับ PMS7003
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-
-#include <Wire.h>
-#include <PMS.h>
-#include <DHT.h>
-#include <HardwareSerial.h>
-
-HardwareSerial SerialPort(1); // use UART1
-PMS pms(SerialPort);
-PMS::DATA data;
-
-#define DHTPIN 26
-#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
-
-DHT dht(DHTPIN, DHTTYPE);
-
-unsigned long previousMillis = 0;
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
+// ชื่อไวไฟกับรหัสผ่าน
 char ssid[] = "SSID";
 char pass[] = "PASS";
 
-BLYNK_CONNECTED()
+HardwareSerial SerialPort(1); // ใช้งาน UART1
+PMS pms(SerialPort);          // สร้าง object ชื่อ pms สำหรับเซนเซอร์ฝุ่น
+PMS::DATA data;               // สร้างตัวแปร data เก็บค่าเซนเซอร์ฝุ่น
+
+#define DHTPIN 26     // ตั้งค่าขาต่อ DHT
+#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
+
+DHT dht(DHTPIN, DHTTYPE); // สร้าง object ชื่อ dht
+
+unsigned long previousMillis = 0; // ตัวแปรเก็บค่าการ delay
+
+BLYNK_CONNECTED() // เรียกเมื่อ blynk เชื่อมต่อสำเร็จ
 {
   Blynk.syncAll();
 }
 
-BLYNK_WRITE(V3)
+BLYNK_WRITE(V3) // เรียกเมือกด Virtual Pin 3 (OFF-ON)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(16, LOW );
-    delay(500);
-    digitalWrite(16, HIGH);
-    Blynk.virtualWrite(V3, 0);
+    digitalWrite(16, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(16, HIGH);    // output เป็น high
+    Blynk.virtualWrite(V3, 0); // อัพเดทค่า Virtual pin
   }
 }
-BLYNK_WRITE(V4)
+BLYNK_WRITE(V4) // เรียกเมือกด Virtual Pin 4 (Timer)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(17, LOW );
-    delay(500);
-    digitalWrite(17, HIGH);
-    Blynk.virtualWrite(V4, 0);
+    digitalWrite(17, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(17, HIGH);    //  เป็น high
+    Blynk.virtualWrite(V4, 0); // อัพเดทค่า Virtual pin
   }
 }
-BLYNK_WRITE(V5)
+BLYNK_WRITE(V5) // เรียกเมือกด Virtual Pin 5 (Mode)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(32, LOW );
-    delay(500);
-    digitalWrite(32, HIGH);
-    Blynk.virtualWrite(V5, 0);
+    digitalWrite(32, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(32, HIGH);    //  เป็น high
+    Blynk.virtualWrite(V5, 0); // อัพเดทค่า Virtual pin
   }
 }
-BLYNK_WRITE(V6)
+BLYNK_WRITE(V6) // เรียกเมือกด Virtual Pin 6 (Max Speed)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(33, LOW );
-    delay(500);
-    digitalWrite(33, HIGH);
-    Blynk.virtualWrite(V6, 0);
+    digitalWrite(33, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(33, HIGH);    //  เป็น high
+    Blynk.virtualWrite(V6, 0); // อัพเดทค่า Virtual pin
   }
 }
-BLYNK_WRITE(V7)
+BLYNK_WRITE(V7) // เรียกเมือกด Virtual Pin 7 (Speed)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(18, LOW );
-    delay(500);
-    digitalWrite(18, HIGH);
-    Blynk.virtualWrite(V7, 0);
+    digitalWrite(18, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(18, HIGH);    //  เป็น high
+    Blynk.virtualWrite(V7, 0); // อัพเดทค่า Virtual pin
   }
 }
-BLYNK_WRITE(V8)
+BLYNK_WRITE(V8) // เรียกเมือกด Virtual Pin 8 (Anion)
 {
-  if (param.asInt() == 1)
+  if (param.asInt() == 1) // ถ้าค่าจาก Blynk เป็น1 (มีการกดปุ่ม)
   {
-    digitalWrite(19, LOW );
-    delay(500);
-    digitalWrite(19, HIGH);
-    Blynk.virtualWrite(V8, 0);
+    digitalWrite(19, LOW);     // output เป็น low (relay active low)
+    delay(500);                // รอ 0.5 วินาที
+    digitalWrite(19, HIGH);    //  เป็น high
+    Blynk.virtualWrite(V8, 0); // อัพเดทค่า Virtual pin
   }
 }
-
 
 void setup()
 {
   // Debug console
-  Serial.begin(115200);
-  SerialPort.begin(9600, SERIAL_8N1, 25, 27);
+  Serial.begin(115200);                       // Serail ทำงาน
+  SerialPort.begin(9600, SERIAL_8N1, 25, 27); // Hardware Serial Port 1 ทำงาน ใช้สือสารกับ PMS7003
 
-  dht.begin();
+  dht.begin(); // dht ทำงาน
 
+  // เรื่องต้นให้ปิด relay (Active low)
   digitalWrite(16, HIGH);
   digitalWrite(17, HIGH);
   digitalWrite(18, HIGH);
@@ -131,38 +135,42 @@ void setup()
   pinMode(32, OUTPUT);
   pinMode(33, OUTPUT);
 
+  // เชือมต่อกับ Blynk
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop()
 {
-  Blynk.run();
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 2500) {
-    previousMillis = currentMillis;
+  Blynk.run(); // blynk
 
-    float humid = dht.readHumidity();
-    float temp = dht.readTemperature();
+  unsigned long currentMillis = millis();     // เก็บค่าเวลาปัจจุบัน
+  if (currentMillis - previousMillis >= 2500) // เทียบเวลาปัจจุบันกับการทำงานครั้งก่อน ให้ทำงานทุก 2500 ms
+  {
+    previousMillis = currentMillis; // อัพเดทเวลาการทำงานล่าสุด
 
+    float humid = dht.readHumidity();   // เรียกค่าความชื้น
+    float temp = dht.readTemperature(); // เรียกค่าอุณหภูมิ
+
+    // แสดงค่าทาง Serial
     Serial.print(F("Humidity: "));
     Serial.print(humid);
     Serial.print(F("%  Temperature: "));
     Serial.print(temp);
     Serial.println(F("°C "));
 
+    // อัพเดทค่าขึ้น Blynk
     Blynk.virtualWrite(V1, temp);
     Blynk.virtualWrite(V2, humid);
-
   }
 
-  //------get data from PMS7003------
+  //------เมื่อเซนเซอร์ส่งค่ามา------
   if (pms.read(data))
   {
-    Blynk.virtualWrite(V0, data.PM_AE_UG_2_5);
+    Blynk.virtualWrite(V0, data.PM_AE_UG_2_5); // อัพเดทค่า pm2.5 ไปยัง virtual pin 0
+
+    // แสดงค่าทาง Serial
     Serial.print(F("PM2.5: "));
     Serial.print(data.PM_AE_UG_2_5);
     Serial.println(F("ug/m3 "));
   }
-
-
 }
--- pmmeter_DHT/pmmeter_DHT.ino
+++ pmmeter_DHT/pmmeter_DHT.ino
@@ -1,67 +1,73 @@
-/*
-   -PMS7003-
-   5V - VCC
-   GND - GND
-   D4  - TX
-   D3  - RX(not use in this code)
+/*************************************************************
+* สำหรับบอร์ด ESP8266
+* 
+* การต่อสาย
+* -PMS7003-
+* 5V - VCC
+* GND - GND
+* D4  - TX
+* D3  - RX(not use in this code)
+*
+* ไลบรารี่เพิ่มเติม
+* 1.PMS โหลดจาก library manager
+* 2.DHT โหลดจาก library manager
+* 3.blynk โหลดจาก library manager
 
-*/
+*************************************************************/
 
-/* Comment this out to disable prints and save space */
+// ดีบัก Blynk ผ่าน Serial
 #define BLYNK_PRINT Serial
 
-/* Fill in information from Blynk Device Info here */
+/* ข้อมูลจาก Blynk */
 #define BLYNK_TEMPLATE_ID "TMPLHZXTQTqu"
 #define BLYNK_TEMPLATE_NAME "Air quality"
 #define BLYNK_AUTH_TOKEN "f4FQUVZN0zuZ7F49CbB8KJt7yTreeDUf"
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#include <Wire.h>
-#include <SFE_MicroOLED.h>
-#include <PMS.h>
-#include <DHT.h>
-#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>        // wifi
+#include <BlynkSimpleEsp8266.h> // blynk
+#include <Wire.h>               // i2c กับจอ oled
+#include <SFE_MicroOLED.h>      // จอ oled
+#include <PMS.h>                // PMS7003 sensor
+#include <DHT.h>                // dht
+#include <SoftwareSerial.h>     // Uart กับ PMS7003
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+// ชื่อไวไฟกับรหัสผ่าน
 char ssid[] = "SSID";
 char pass[] = "PASS";
 
-SoftwareSerial pmsSerial(D4, D3); // RX,TX
-PMS pms(pmsSerial);
-PMS::DATA data;
+SoftwareSerial pmsSerial(D4, D3); // ใช้งาน software serial RX,TX ที่ขา D4,D3
+PMS pms(pmsSerial);               // สร้าง object ชื่อ pms สำหรับเซนเซอร์ฝุ่น
+PMS::DATA data;                   // สร้างตัวแปร data เก็บค่าเซนเซอร์ฝุ่น
 
-#define DHTPIN D7
-// Uncomment whatever type you're using!
-// #define DHTTYPE DHT11 // DHT 11
+#define DHTPIN D7     // ตั้งค่าขาต่อ DHT
 #define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-DHT dht(DHTPIN, DHTTYPE);
-uint8_t dht_time;
+DHT dht(DHTPIN, DHTTYPE); // สร้าง object ชื่อ dht
 
-float humid, temp;
+float humid, temp; // ตัวแปรเก็บค่า humid temperature
 
+// ตั้งค่าการทำงานจอ oled
 #define PIN_RESET -1
 #define DC_JUMPER 0
 MicroOLED oled(PIN_RESET, DC_JUMPER);
 
-unsigned long previousMillis = 0;
+unsigned long previousMillis = 0; // ตัวแปรเก็บค่าการ delay
 
 void setup()
 {
-  Serial.begin(115200);
-  pmsSerial.begin(9600);
-  dht.begin();
-  Wire.begin();
+  Serial.begin(115200);  // Serail ทำงาน
+  pmsSerial.begin(9600); // Software Serial ทำงาน ใช้สือสารกับ PMS7003
 
+  dht.begin();  // dht ทำงาน
+  Wire.begin(); // i2c ทำงาน เพื่อสื่อสารกับจอ oled
+
+  // จ่ายไฟให้ dht
   pinMode(D6, OUTPUT);
   pinMode(D8, OUTPUT);
   digitalWrite(D6, HIGH);
   digitalWrite(D8, LOW);
 
-  //------Display LOGO at start------
+  // เรื่มต้นทำงาน จอ oled
   oled.begin();
   oled.clear(PAGE);
   oled.clear(ALL);
@@ -70,63 +76,65 @@ void setup()
   oled.print("PMMeter");
   oled.display();
 
+  // เชือมต่อกับ Blynk
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop()
 {
 
-  Blynk.run();
+  Blynk.run(); // blynk
 
-  //------get data from PMS7003------
+  //------ เมื่อเซนเซอร์ PMS7003 ส่งค่ามา ------
   if (pms.read(data))
   {
-    Blynk.virtualWrite(V9, data.PM_AE_UG_2_5);
+    Blynk.virtualWrite(V9, data.PM_AE_UG_2_5); // ส่งค่า pm2.5 ไป V9
+
+    // แสดงค่าฝุ่นทาง Serial monitor
     Serial.print(F("PM2.5: "));
     Serial.print(data.PM_AE_UG_2_5);
     Serial.println(F("ug/m3 "));
   }
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 2500)
-  { // run every 1 second
-    previousMillis = currentMillis;
+  unsigned long currentMillis = millis();     // เก็บค่าเวลาปัจจุบัน
+  if (currentMillis - previousMillis >= 2500) // เทียบเวลาปัจจุบันกับการทำงานครั้งก่อน ให้ทำงานทุก 2500 ms
+  {
+    previousMillis = currentMillis; // อัพเดทเวลาการทำงานล่าสุด
 
-    humid = dht.readHumidity();
-    temp = dht.readTemperature();
+    humid = dht.readHumidity();   // เรียกค่าความชื้น
+    temp = dht.readTemperature(); // เรียกค่าอุณหภูมิ
 
+    // อัพเดทค่าขึ้น Blynk
     Blynk.virtualWrite(V10, humid);
     Blynk.virtualWrite(V11, temp);
 
+    // แสดงค่าทาง Serial
     Serial.print(F("Humidity: "));
     Serial.print(humid);
     Serial.print(F("%  Temperature: "));
     Serial.print(temp);
     Serial.println(F("°C "));
 
-    display_update();
+    display_update(); // แสดงค่าขึ้นจอ oled
   }
-
-
 }
 
-void display_update()
+void display_update() // แสดงค่าบนจอ oled
 {
+ 
+  oled.clear(PAGE);// เครียร์บนจอ
+  oled.setFontType(0);// เลือก font
+  oled.setCursor(0, 0);// เลือกตำแหน่ง
+  oled.println("-Sensors-");// แสดงตัวอักษร
+  oled.setCursor(0, 15);// เลือกตำแหน่ง
+  oled.print("PM2.5: ");// แสดงตัวอักษร
+  oled.print(data.PM_AE_UG_2_5, 0);// แสดงค่า pm2.5
+  oled.setCursor(0, 27);// เลือกตำแหน่ง
+  oled.print("T: ");// แสดงตัวอักษร
+  oled.print(temp, 1);// แสดงค่าอุณหภูมิ
+  oled.setCursor(0, 39);// เลือกตำแหน่ง
+  oled.print("H: ");// แสดงตัวอักษร
+  oled.print(humid, 1);// แสดงค่าความชื้น
 
-  oled.clear(PAGE);
-  oled.setFontType(0);
-  oled.setCursor(0, 0);
-  oled.println("-Sensors-");
-  oled.setCursor(0, 15);
-  oled.print("PM2.5: ");
-  oled.print(data.PM_AE_UG_2_5, 0);
-  oled.setCursor(0, 27);
-  oled.print("T: ");
-  oled.print(temp, 1);
-  oled.setCursor(0, 39);
-  oled.print("H: ");
-  oled.print(humid, 1);
-
-  oled.display();
-
+  oled.display(); // อัพเดทค่าบนจอ oled
 }
